a4f4b97e27518e021a9fd094f09a3cbacc10450a
Ivan <ivan_semenov@bk.ru>
2022-05-22 10:59:47 +0300

    Initial commit

---
+++ trainingCorse_hibernate/src/test/java/ru/geekbrains/isemenov/trainingCorse_hibernate/TrainingCorseHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.isemenov.trainingCorse_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingCorseHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
